6f95e7b80a8217f1e470fb320e50bbbb5b235df8
atillakokcu <kokcu.ati@gmail.com>
2022-12-15 00:13:13 +0300

    Token oluşturma, aes crypt, security

--- PYS.Application.Business/KullaniciIslemleri.cs
+++ PYS.Application.Business/KullaniciIslemleri.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PYS.Application.Data;
+using PYS.Application.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace PYS.Application.Business
 {
-    internal class KullaniciIslemleri
+    internal class KullaniciIslemleri:BaseKullaniciIslemleri
     {
+        public string GetToken(string KullaniciBilgisi, string Sifre,out string Mesaj)
+        {
+            VwKisiKullaniciIletisim KullaniciKisi = null;
+            string Result = "";
+            
+            bool Success= base.DoLogin(KullaniciBilgisi, Sifre,out KullaniciKisi,out Mesaj);
+
+            if (Success)
+            {
+                if (KullaniciKisi!=null)
+                {
+                    Result = KullaniciKisi.Guid.Value + "|" +
+                                            KullaniciKisi.FirmaKodu + "|" +
+                                            KullaniciKisi.KisiId + "|" +
+                                            KullaniciKisi.Tc + "|" +
+                                            KullaniciKisi.KullaniciAdi + "|" +
+                                            KullaniciKisi.KullaniciId + "|" +
+                                            Guid.NewGuid().ToString();
+
+                    Result = PYSSecurity.Encrypt(Result, KullaniciKisi.Guid.Value.ToString());
+                }
+                
+
+            }
+            return Result;
+        }
+
+
     }
 }
---
+++ PYS.Application.Security.TestForm/PYS.Application.Security.TestForm/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PYS.Application.Security;
+
+namespace PYS.Application.Security.TestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           TxtEncryptText.Text= PYSSecurity.Encrypt(TxtCleantext.Text.ToString(), "asdasdas");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtDecryptText.Text = PYSSecurity.Decrypt(TxtEncryptText.Text, "asdsa");
+        }
+    }
+}
